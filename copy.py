a5dbe2cd44b53a8940c48872296e037a0ee1290d
Abraham Fong <abraham@parlevelsystems.com>
2022-01-13 19:46:58 -0600

    clean up code

--- copy.py
+++ copy.py
@@ -1,18 +1,5 @@
 #!/usr/bin/env python3
 
-import re
-import os
-import subprocess
-import paramiko
-from dotenv import load_dotenv
-from pathlib import Path
-
-
-from scp import SCPClient
-import glob
-
-
-
 # requred for this lab
 # - VM WARE
 # - UBUNTU ISO
@@ -23,6 +10,16 @@ import glob
 # need to install dotenv and pathlib to read from env file
 # need to install paramiko using pip3 (used to SSH into servers)
 # need to install scp using pip3 (used to copy files) https://pypi.org/project/scp/
+
+import re
+import os
+import subprocess
+import paramiko
+from dotenv import load_dotenv
+from pathlib import Path
+from scp import SCPClient
+import glob
+
 
 def getCredentials():
 
@@ -42,56 +39,62 @@ def getCredentials():
     # TEST SERVER STATUS
     response1 = os.system("ping -c 1 " + server_one)
     response2 = os.system("ping -c 1 " + server_two)
-
-    # TEST TO SEE IF REMOTE SERVERS ARE UP
     if not response1 == 0:
         print (server_one, 'is not up!')
         exit 
     if not response2 == 0:
-        print (server_two, 'is up!')
+        print (server_two, 'is not up!')
         exit 
 
     return user, pw, server_one, server_two, port
 
-user, pw, server_one, server_two, port = getCredentials()
 
 
-# CONNECT TO SERVER ONE
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(source_host, port, username, password)
+def sshConnectCopy(source_host,port,username,password):
+    # set up ssh client
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(source_host, port, username, password)
 
-# LIST ALL FILES TO BE COPIED FROM SOURCE
-command = "ls ./files"
-stdin, stdout, stderr = ssh.exec_command(command)
-files = stdout.readlines()
+    # get files
+    command = "ls ./files"
+    stdin, stdout, stderr = ssh.exec_command(command)
+    files = stdout.readlines()
 
-# COPY ALL FILES FOUND TO LOCAL MACHINE
-with SCPClient(ssh.get_transport()) as scp:
-    for file in files:
-        scp.get("~/files/"+file.rstrip("\n"))
-
-files_to_copy = [f for f in glob.glob("move*.txt")]
-print(files_to_copy)
+    # COPY ALL FILES FOUND TO LOCAL MACHINE
+    with SCPClient(ssh.get_transport()) as scp:
+        for file in files:
+            scp.get("~/files/"+file.rstrip("\n"))
 
 
-# SSH INTO DESTINATION
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(dest_host, port, username, password)
+def sshConnectTransfer(dest_host,port,username,password):
+    # SSH INTO DESTINATION
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(dest_host, port, username, password)
 
-# MAKE A DIRECTORY TO COPY FILES INTO IN DESTINATION
-command = "mkdir ./files"
-stdin, stdout, stderr = ssh.exec_command(command)
+    # MAKE A DIRECTORY TO COPY FILES INTO IN DESTINATION
+    command = "mkdir ./files"
+    stdin, stdout, stderr = ssh.exec_command(command)
 
-# COPY ALL FILES FROM LOCAL TO DESTINATION SERVER
-with SCPClient(ssh.get_transport()) as scp:
-    for file in files_to_copy:
-        scp.put("~/files/"+file)
+    # COPY ALL FILES FROM LOCAL TO DESTINATION SERVER
+    with SCPClient(ssh.get_transport()) as scp:
+        for file in files_to_copy:
+            scp.put(file, recursive=True, remote_path='./files')
 
 
-# TODO
-# COPY FILES INTOT ./files IN DESTINATION
-# USE user AND pw READ IN EARLIER FROM TXT FILE
-# ADD ERROR CHECKING
-# CLEAN UP
+if __name__ == "__main__":
+    # GRAB CREDENTIALS
+    user, pw, source_host, destination_host, port = getCredentials()
+
+    # COPY FILES FROM REMOTE SERVER A
+    sshConnectCopy(source_host, port, user, pw)
+
+    #  files from local
+    files_to_copy = [f for f in glob.glob("move*.txt")]
+    print("FILES TO COPY:", files_to_copy)
+
+    # TRANSFER FILES TO REMOTE SERVER B
+    sshConnectTransfer(destination_host, port, user, pw)
+
+    print("DONE")
